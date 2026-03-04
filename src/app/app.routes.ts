cb5e87a7e085513508800aa053a019d2d7f45419
bonotan_k <bonotan_k@ca01.ub.lan>
2026-03-04 16:21:27 +0800

    Hoenn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { KantoComponent } from './Kanto/kanto.component';
+import { JohtoComponent } from './johto/johto.component';
+import { HoennComponent } from './hoenn/hoenn.component';
+import { HomeComponent } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'kanto', component: KantoComponent },
+  { path: 'johto', component: JohtoComponent },
+  { path: 'hoenn', component: HoennComponent },
+];
+
+
